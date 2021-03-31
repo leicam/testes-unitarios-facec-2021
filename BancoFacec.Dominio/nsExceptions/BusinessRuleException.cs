2d267fb24adaa5593d8e2f966b391730ce555481
Juliano Ribeiro de Souza Maciel <juliano.maciel@totvs.com.br>
2021-03-30 21:29:43 -0300

    Implementação do projeto FACEC Bank + Testes Unitários

---
+++ BancoFacec.Dominio/nsExceptions/BusinessRuleException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoFacec.Dominio.nsExceptions
+{
+    public class BusinessRuleException : Exception
+    {
+        public BusinessRuleException() { }
+        public BusinessRuleException(string message) : base(message) { }
+    }
+}
